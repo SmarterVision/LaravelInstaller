b561c8098cfde31ef0d6b7cf3353531b22e130ca
SmarterVision <smartersvision@gmail.com>
2021-08-11 11:24:21 +0100

    Fix .env generated file

--- src/Helpers/EnvironmentManager.php
+++ src/Helpers/EnvironmentManager.php
@@ -102,28 +102,27 @@ class EnvironmentManager
         'APP_LOG_LEVEL=' . $request->app_log_level . "\n" .
         'APP_URL=' . $request->app_url . "\n\n" .
         'PURCHASE_CODE=' . $request->purchase_code . "\n\n" .
+        'APP_TIMEZONE=Europe/Berlin' . "\n" .
+        '# more details about timezone: https://www.php.net/manual/en/timezones.asia.php' . "\n\n" .
         'DB_CONNECTION=' . $request->database_connection . "\n" .
         'DB_HOST=' . $request->database_hostname . "\n" .
         'DB_PORT=' . $request->database_port . "\n" .
         'DB_DATABASE=\'' . $request->database_name . "'\n" .
         'DB_USERNAME=\'' . $request->database_username . "'\n" .
         'DB_PASSWORD=\'' . $request->database_password . "'\n\n" .
-        'BROADCAST_DRIVER='.$request->broadcast_driver."\n".
-        'CACHE_DRIVER='.$request->cache_driver."\n".
-        'SESSION_DRIVER='.$request->session_driver."\n".
-        'QUEUE_DRIVER='.$request->queue_driver."\n\n".
-        'REDIS_HOST='.$request->redis_hostname."\n".
-        'REDIS_PASSWORD='.$request->redis_password."\n".
-        'REDIS_PORT='.$request->redis_port."\n\n".
-        'MAIL_DRIVER='.$request->mail_driver."\n".
-        'MAIL_HOST='.$request->mail_host."\n".
-        'MAIL_PORT='.$request->mail_port."\n".
-        'MAIL_USERNAME='.$request->mail_username."\n".
-        'MAIL_PASSWORD='.$request->mail_password."\n".
-        'MAIL_ENCRYPTION='.$request->mail_encryption."\n\n".
-        'PUSHER_APP_ID='.$request->pusher_app_id."\n".
-        'PUSHER_APP_KEY='.$request->pusher_app_key."\n".
-        'PUSHER_APP_SECRET='.$request->pusher_app_secret;
+        'BROADCAST_DRIVER=log' . "\n" .
+        'CACHE_DRIVER=file' . "\n" .
+        'QUEUE_CONNECTION=sync' . "\n" .
+        'SESSION_DRIVER=file' . "\n\n" .
+        'REDIS_HOST=' . $request->redis_hostname . "\n" .
+        'REDIS_PASSWORD=' . $request->redis_password . "\n" .
+        'REDIS_PORT=' . $request->redis_port . "\n\n" .
+        'MAIL_DRIVER=' . $request->mail_driver . "\n" .
+        'MAIL_HOST=' . $request->mail_host . "\n" .
+        'MAIL_PORT=' . $request->mail_port . "\n" .
+        'MAIL_USERNAME=' . $request->mail_username . "\n" .
+        'MAIL_PASSWORD=' . $request->mail_password . "\n" .
+        'MAIL_ENCRYPTION=' . $request->mail_encryption . "\n\n";
 
         try {
             file_put_contents($this->envPath, $envFileData);
