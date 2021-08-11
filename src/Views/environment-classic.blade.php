8bbdd230009fbedb682f5c0df1fdaf16ad393bd0
SmarterVision <smartersvision@gmail.com>
2021-08-11 10:14:16 +0100

    Fix Views

--- src/Views/environment-classic.blade.php
+++ src/Views/environment-classic.blade.php
@@ -15,23 +15,19 @@
         <textarea class="textarea" name="envConfig">{{ $envConfig }}</textarea>
         <div class="buttons buttons--right">
             <button class="button button--light" type="submit">
-            	<i class="fa fa-floppy-o fa-fw" aria-hidden="true"></i>
-             	{!! trans('installer_messages.environment.classic.save') !!}
+                <i class="fa fa-floppy-o fa-fw" aria-hidden="true"></i>
+                {!! trans('installer_messages.environment.classic.save') !!}
             </button>
         </div>
     </form>
 
     @if( ! isset($environment['errors']))
         <div class="buttons-container">
-            <a class="button float-left" href="{{ route('LaravelInstaller::environmentWizard') }}">
-                <i class="fa fa-sliders fa-fw" aria-hidden="true"></i>
+            <a class="button float-left" href="{{ route('LaravelInstaller::environmentWizard') }}"> <i class="fa fa-sliders fa-fw" aria-hidden="true"></i>
                 {!! trans('installer_messages.environment.classic.back') !!}
-            </a>
-            <a class="button float-right" href="{{ route('LaravelInstaller::database') }}">
-                <i class="fa fa-check fa-fw" aria-hidden="true"></i>
+            </a> <a class="button float-right" href="{{ route('LaravelInstaller::database') }}"> <i class="fa fa-check fa-fw" aria-hidden="true"></i>
                 {!! trans('installer_messages.environment.classic.install') !!}
-                <i class="fa fa-angle-double-right fa-fw" aria-hidden="true"></i>
-            </a>
+                <i class="fa fa-angle-double-right fa-fw" aria-hidden="true"></i> </a>
         </div>
     @endif
 
--- src/Views/permissions.blade.php
+++ src/Views/permissions.blade.php
@@ -13,22 +13,20 @@
 
     <ul class="list">
         @foreach($permissions['permissions'] as $permission)
-        <li class="list__item list__item--permissions {{ $permission['isSet'] ? 'success' : 'error' }}">
-            {{ $permission['folder'] }}
-            <span>
-                <i class="fa fa-fw fa-{{ $permission['isSet'] ? 'check-circle-o' : 'exclamation-circle' }}"></i>
-                {{ $permission['permission'] }}
-            </span>
-        </li>
+            <li class="list__item list__item--permissions {{ $permission['isSet'] ? 'success' : 'error' }}">
+                {{ $permission['folder'] }}
+                <span> <i class="fa fa-fw fa-{{ $permission['isSet'] ? 'check-circle-o' : 'exclamation-circle' }}"></i>
+                    {{ $permission['permission'] }}
+                </span>
+            </li>
         @endforeach
     </ul>
 
     @if ( ! isset($permissions['errors']))
         <div class="buttons">
-            <a href="{{ route('LaravelInstaller::environment') }}" class="button">
+            <a href="{{ route('LaravelInstaller::environmentWizard') }}" class="button">
                 {{ trans('installer_messages.permissions.next') }}
-                <i class="fa fa-angle-right fa-fw" aria-hidden="true"></i>
-            </a>
+                <i class="fa fa-angle-right fa-fw" aria-hidden="true"></i> </a>
         </div>
     @endif
 
